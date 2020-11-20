1cd1d46b238320a6a79853c5923b97a559dc3b95
Porut Иван <kegont50@gmail.com>
2020-11-20 19:01:57 +0700

    1.Создано приложение с шаблоном EmptyActivity 2. Выполнен рефакторинг: MainActivity -> PhotoGallery, activity_main -> gallery_activity.

---
+++ app/src/main/java/com/example/gallery/PhotoGallery.java
@@ -0,0 +1,13 @@
+package com.example.gallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhotoGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gallery_activity);
+    }
+}
