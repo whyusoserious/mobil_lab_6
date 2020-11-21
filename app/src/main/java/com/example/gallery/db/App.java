7b13f451741c17fd3ffa1d85f0435052b16a2bfa
Porut Иван <kegont50@gmail.com>
2020-11-22 02:21:25 +0700

    8.	9.	Добавьте папку db и переопределите в ней интерфейс DAO, абстрактный класс для вашей БД и класс для работы с базой данных

---
+++ app/src/main/java/com/example/gallery/db/App.java
@@ -0,0 +1,26 @@
+package com.example.gallery.db;
+
+import android.app.Application;
+import androidx.room.Room;
+
+public class App extends Application {
+    public static App instance;
+
+    private PhotosDB database;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        database = Room.databaseBuilder(this, PhotosDB.class, "database").allowMainThreadQueries()
+                .build();
+    }
+
+    public static App getInstance() {
+        return instance;
+    }
+
+    public PhotosDB getDatabase() {
+        return PhotosDB.getDatabase(getApplicationContext());
+    }
+}
