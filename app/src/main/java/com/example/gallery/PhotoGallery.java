27c1051d811670b1e34cbec0fd84772cdc258559
Shcherbakov_BPA18-01 <kegont50@gmail.com>
2020-11-21 19:44:08 +0700

    6.	Добавьте в код PhotoGallery объект RecyclerView для списка фотографий и создайте класс PhotoAdapter для реализации RecyclerView. В качестве LayoutManager для RecyclerView установите GridLayoutManager (таблица с 3 столбцами).

--- app/src/main/java/com/example/gallery/PhotoGallery.java
+++ app/src/main/java/com/example/gallery/PhotoGallery.java
@@ -2,6 +2,8 @@ package com.example.gallery;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PhotoGallery extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class PhotoGallery extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery_activity);
+
+        final RecyclerView rv = findViewById(R.id.recView);
+        rv.setLayoutManager(new GridLayoutManager(this, 3));
     }
 }
